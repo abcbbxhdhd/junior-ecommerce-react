2a0caf611a51f40b2b89110385f1dda299e7963c
abcbbxhdhd <ajax76@protonmail.com>
2022-03-26 18:34:50 +0200

    Added router for views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react"
+import { Routes, Route } from "react-router";
+import Cart from "./views/Cart";
+import Mainpage from "./views/Mainpage";
+import ProductDetail from "./views/ProductDetail";
 
 function App() {
 
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Mainpage />}/>
+      <Route path="/:productId" element={<ProductDetail />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import {ApolloProvider} from "@apollo/client"
 import client from "./common/apollo-client"
 import { Provider } from 'react-redux';
 import store from "./redux/index"
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </Provider>,
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ApolloProvider>
+    </Provider>,
   document.getElementById('root')
 );
