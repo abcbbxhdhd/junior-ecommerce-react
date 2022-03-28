4ae3ca0345572a1ebfaf0aeb5e611c69b182bdd6
abcbbxhdhd <ajax76@protonmail.com>
2022-03-28 21:47:13 +0300

    Added cart amount label in header

--- src/components/header/CartOverlay.js
+++ src/components/header/CartOverlay.js
@@ -1,9 +1,25 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 export default function CartOverlay() {
+    const cartProducts = useSelector(state => state.products)
+
+    const productAmount = () => {
+        let value = 0;
+        cartProducts.forEach(product => {
+            value += product.amount
+        });
+        return value
+    }
+
+    const amount = productAmount()
+
     return (
         <div className="cart-overlay">
             <i className="ri-shopping-cart-2-line"></i>
+            { amount > 0 && <div className="product-amount-badge">
+                <h4>{amount}</h4>
+            </div>}
         </div>
     )
 }
