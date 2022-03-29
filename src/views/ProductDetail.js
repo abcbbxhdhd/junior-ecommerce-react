f876d8e0678f83072b2176777fe2582209ca643e
abcbbxhdhd <ajax76@protonmail.com>
2022-03-29 13:41:13 +0300

    Updated Product detail view

--- src/views/ProductDetail.js
+++ src/views/ProductDetail.js
@@ -1,7 +1,39 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router"
+import { GET_PRODUCT_BY_ID } from "../common/queries"
+import { useQuery } from "@apollo/client"
+import ImageCarousel from "../components/product-detail/ImageCarousel"
+import ProductFeatures from "../components/product-detail/ProductFeatures"
+import Header from "../components/header/Header"
+import "../components/product-detail/ProductDetail.css"
 
 export default function ProductDetail() {
+    const {productId} = useParams()
+    const {data, loading, error} = useQuery(GET_PRODUCT_BY_ID, {
+        variables: {
+            productId
+        }
+    })
+    const [product, setProduct] = useState()
+
+    useEffect(() => {
+        if (data) {
+            setProduct(data.product)
+        }
+    }, [data])
+
+    if (!product) {
+        return <h1>Loading...</h1>
+    }
+
     return (
-        <h1>Product Detail</h1>
+        <>
+            <Header />
+            <div className="sections-flex">
+                <ImageCarousel gallery={product.gallery}/>
+                <ProductFeatures product={product}/>
+            </div>
+            </>
     )
+        
 }
